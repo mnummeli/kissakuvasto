fa2b859e88530bf5322be73fce046e68f58adadd
Mikko Nummelin <mnummeli@gmail.com>
2021-08-07 19:52:31 +0300

    w3.css

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@
 'use strict';
 
 const React = require('react');
+const {useReducer, useEffect} = React;
 const ReactDOM = require('react-dom');
 const $ = require('jquery');
+const dateFormat = require('dateformat');
 
 function App(props) {
-    const [state, dispatch] = React.useReducer(reducer, {catNumber: 0, catNames: undefined});
+    const [state, dispatch] = useReducer(reducer, {catNumber: 0, catNames: undefined});
 
-    React.useEffect(() => {
+    useEffect(() => {
         $.getJSON('/kissat.json', data => {
             dispatch({command: 'init', data});
         });
@@ -41,12 +43,39 @@ function App(props) {
     }
 
     return <div>
+        <HeaderComponent/>
         <CatImage n={state.catNumber}/>
-        <h2>{state.catNames ? state.catNames[state.catNumber] : 'Lataa ...'}</h2>
+        <div className="w3-container w3-yellow">
+            <h2>{state.catNames ? state.catNames[state.catNumber] : 'Lataa ...'}</h2>
+        </div>
         <div style={{clear: "both"}}/>
-        <button onClick={() => dispatch({command: '-'})}>Edellinen</button>
-        <button onClick={() => dispatch({command: '+'})}>Seuraava</button>
+        <div className="w3-container w3-red">
+            <button className="w3-button w3-grey w3-margin w3-border"
+                    onClick={() => dispatch({command: '-'})}>Edellinen</button>
+            <button className="w3-button w3-grey w3-margin w3-border"
+                    onClick={() => dispatch({command: '+'})}>Seuraava</button>
+        </div>
     </div>;
+}
+
+function HeaderComponent(props) {
+    return <div className="w3-container w3-black">
+        <h1 className="w3-left">Kissakuvasto</h1>
+        <Clock className="w3-right"/>
+    </div>;
+}
+
+function Clock(props) {
+    const [state, dispatch] = useReducer(state => {
+        return new Date();
+    }, new Date());
+    useEffect(() => {
+        const interval = setInterval(dispatch, 100);
+        return () => {
+            deleteInterval(interval);
+        };
+    }, []);
+    return <h3>{dateFormat(state, "isoDateTime")}</h3>;
 }
 
 function CatImage(props) {
